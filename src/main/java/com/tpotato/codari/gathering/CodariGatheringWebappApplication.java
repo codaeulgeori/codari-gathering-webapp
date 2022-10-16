8edead757848872c6b7c0586b074a0d733e2074b
jhpark-mac <ggoowlgns@gmail.com>
2022-10-16 16:50:53 +0900

    feat(init): spring reactive : 2.7.4

---
+++ src/main/java/com/tpotato/codari/gathering/CodariGatheringWebappApplication.java
@@ -0,0 +1,13 @@
+package com.tpotato.codari.gathering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodariGatheringWebappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodariGatheringWebappApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/tpotato/codari/gathering/CodariGatheringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpotato.codari.gathering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodariGatheringWebappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
